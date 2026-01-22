7c34ba78d88d6e050c04e4f1f35143eb9b73015f
gyannavrielbaybayon-create <gyann.avriel.baybayon@gmail.com>
2026-01-22 18:50:45 +0800

    Update Q3Sw6.java

--- Q3Sw6.java
+++ Q3Sw6.java
@@ -1,1 +1,25 @@
-	              
+            class Student {
+	        String name;
+	        String race;
+	        void displayName() {
+	            System.out.println("Student Name: " + name);
+	            System.out.println("Student Race: " + race);
+	        }
+	    }
+	    
+	   public class main {
+	public static void main(String[] args) {
+	    
+	         Student s1 = null;
+	         Student s2 = new Student();
+	         s2.name = "Joe";
+	         s2.race = "Blasian";
+	         // simple error handling is "try{" and "}catch"
+	         try {
+	              s2.displayName();
+	         } catch (Exception e) {
+	              System.out.println("Error: Student object is not created");
+	         }
+	}
+	   }
+	              	              
