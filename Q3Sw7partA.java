21a082b126ef706bf00229e13663165bb3121426
gyannavrielbaybayon-create <gyann.avriel.baybayon@gmail.com>
2026-01-22 18:54:28 +0800

    Update Q3Sw7partA.java

--- Q3Sw7partA.java
+++ Q3Sw7partA.java
@@ -1,1 +1,17 @@
-
+//Part 1
+public class Q3Sw7partA {
+	public static void main(String[] args) {
+		int number1= 20;
+		int number2= 0;
+		
+		try{
+		    int result = number1 / number2; //causes error
+		    System.out.println("Result; " + result);
+		} catch (ArithmeticException e) {
+		    System.out.println("Built-in Exception: Cannot divide by zero");
+		}
+		
+		System.out.println("Program continues running...");
+				
+	}
+}
