26e05bdf028f3962559d9e9aa5ac2f3cb6d52eeb
gyannavrielbaybayon-create <gyann.avriel.baybayon@gmail.com>
2026-02-10 23:44:53 +0800

    Update Q3Sw9.java

--- Q3Sw9.java
+++ Q3Sw9.java
@@ -1,1 +1,33 @@
-
+class Task1 extends Thread {
+    public void run() {
+        for (int i = 1; i <=5; i++) {
+        System.out.println("Task 1 running: " + i);
+        try{
+            Thread.sleep(500); //pause for 0.5 second
+        } catch (InterruptedException e) {
+            System.out.println(e);
+            }
+        }
+    }
+}
+class Task2 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Task 2 running: " + i);
+        try{
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+            }
+        }
+    }
+}
+// this is ConcurrencyExample
+public class Q3Sw9 {
+	public static void main(String[] args) {
+		Task1 t1 = new Task1();
+		Task2 t2 = new Task2();
+		t1.start(); //starts task 1
+		t2.start(); //starts task 2
+	}
+}
