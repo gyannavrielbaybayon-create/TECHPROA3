061baef8be1d95e8d9be4a3cdfbe3a779e1c13e1
gyannavrielbaybayon-create <gyann.avriel.baybayon@gmail.com>
2026-02-12 10:55:13 +0800

    Update Q3Sw10.java

--- Q3Sw10.java
+++ Q3Sw10.java
@@ -1,1 +1,24 @@
-
+class MyTask implements Runnable {
+    private String taskName;
+    
+    MyTask(String taskName) {
+        this.taskName = taskName;
+    }
+    
+    public void run() {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(taskName + " is running - step " + i);
+            try {
+                Thread.sleep(600);
+            }catch (InterruptedException e) {
+                System.out.println(e);
+            }
+         }
+    }
+}
+public class Q3Sw10 {
+	public static void main(String[] args) {
+	
+	System.out.println("Code complete");
+	}
+}
