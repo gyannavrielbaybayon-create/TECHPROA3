f67506036f894e1045f58bd92f24e7e17c810a24
gyannavrielbaybayon-create <gyann.avriel.baybayon@gmail.com>
2026-02-27 14:12:22 +0800

    Update Q3Sw9Task3.java

--- Q3Sw9Task3.java
+++ Q3Sw9Task3.java
@@ -1,1 +1,26 @@
+import java.util.Random;
 
+public class Q3Sw9Task3 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int number;
+        boolean isHeads;
+
+        // Fixed line 11: Compatible with all Java versions
+        // nextInt(6) gives 0-5; add 1 to get 1-6
+        number = random.nextInt(6) + 1;
+
+        isHeads = random.nextBoolean();
+
+        System.out.println(number);
+
+        if(isHeads){
+            System.out.println("HEADS");
+        }
+        else{
+            System.out.println("TAILS");
+        }
+    }
+}
